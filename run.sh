1a55eb4aca352a1671f93be306c770977c6fb8ee
rplemos <rafael_plemos@hotmail.com>
2024-10-30 20:02:46 -0300

    Removed duplicated scripts

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-today=$(date +"%Y-%m-%d")
-/hd/new/scripts/bash3.sh | tee -a /hd/new/logs/log_${today}.log
-echo -e "\nDatabase update complete!\n" | tee -a /hd/new/logs/log_${today}.log
